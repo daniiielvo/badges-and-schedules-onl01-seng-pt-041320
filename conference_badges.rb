a9c738e28ab91f189c4813f7a4c47728c2f0da34
Daniel Vo <daniiielvo@gmail.com>
2020-04-20 19:16:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker(name)
   return "Hello my name is #{name}"
 end
   
-  batch_badge_creator = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
+  batch_badge_creator(Attendees)
   return 
 end
 
